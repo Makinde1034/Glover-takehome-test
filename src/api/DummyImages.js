a6e5ae68e333ef3b8ff29b151009e0148415dab1
Makinde1034 <makinde1034@gmail.com>
2022-03-24 10:15:25 +0100

    .

--- src/api/DummyImages.js
+++ src/api/DummyImages.js
@@ -102,5 +102,9 @@ export const images = [
     {
         url:"https://images.unsplash.com/photo-1549834125-82d3c48159a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fG11c2ljaWFuc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
         title : "Unbottle"
+    },
+    {
+        url:"https://images.unsplash.com/photo-1619379044287-3c7d04199c8b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTd8fG11c2ljaWFuc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+        title : "rednecks "
     }
 ]
